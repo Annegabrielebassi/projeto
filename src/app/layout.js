e2be623712bf98efaa390bcbdb9458e58ee6e802
hiagogabrielga <160147351+Annegabrielebassi@users.noreply.github.com>
2025-02-09 12:01:19 -0400

    atualizar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 export const metadata = {
   title: "ANNE GABRIELE",
@@ -21,9 +12,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="pt-br">
+      <body>
+        <header>
+          <Header/>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          <Footer/>
+        </footer>
       </body>
     </html>
   );
---
+++ src/components/footer/index.js
@@ -0,0 +1,14 @@
+"use client"
+import Link from "next/link"
+import style from "./footer.module.css"
+import { useState } from "react";
+
+export default function Footer() {
+    return (
+        <div>
+            <h2>
+                footer
+            </h2>
+        </div>
+    )
+}
